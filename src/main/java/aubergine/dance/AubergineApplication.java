051c08ec8d61270375cd0871ac2f99ca24d97f48
Donghoon Lee <aru0504@naver.com>
2024-11-09 20:39:35 +0900

    init: initialize project

---
+++ src/main/java/aubergine/dance/AubergineApplication.java
@@ -0,0 +1,12 @@
+package aubergine.dance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AubergineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AubergineApplication.class, args);
+    }
+}
